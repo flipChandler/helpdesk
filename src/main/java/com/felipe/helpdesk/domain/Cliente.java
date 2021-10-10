e32526a2746b761e37c72d1732becda2574e68e7
flipChandler <felipe.santos11@hotmail.com>
2021-10-09 22:21:46 -0300

    anotacoes jpa para classes de dominio

--- src/main/java/com/felipe/helpdesk/domain/Cliente.java
+++ src/main/java/com/felipe/helpdesk/domain/Cliente.java
@@ -3,16 +3,27 @@ package com.felipe.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.felipe.helpdesk.domain.enums.Perfil;
+
+@Entity(name = "clientes")
 public class Cliente extends Pessoa {	
 	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")  // um cliente para n chamados
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/felipe/helpdesk/domain/Tecnico.java
+++ src/main/java/com/felipe/helpdesk/domain/Tecnico.java
@@ -3,16 +3,27 @@ package com.felipe.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.felipe.helpdesk.domain.enums.Perfil;
+
+@Entity(name = "tecnicos")
 public class Tecnico extends Pessoa {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico") // um tecnico para n chamados | "tecnico" é o objeto que está em Chamado
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.TECNICO);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.TECNICO);
 	}
 
 	public List<Chamado> getChamados() {
