e81528adf0ff3c6c5468886068772f2ca872af51
flipChandler <felipe.santos11@hotmail.com>
2021-10-09 23:43:06 -0300

    add perfil de teste

--- src/main/java/com/felipe/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/felipe/helpdesk/HelpdeskApplication.java
@@ -1,52 +1,12 @@
 package com.felipe.helpdesk;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.felipe.helpdesk.domain.Chamado;
-import com.felipe.helpdesk.domain.Cliente;
-import com.felipe.helpdesk.domain.Tecnico;
-import com.felipe.helpdesk.domain.enums.Perfil;
-import com.felipe.helpdesk.domain.enums.Prioridade;
-import com.felipe.helpdesk.domain.enums.Status;
-import com.felipe.helpdesk.repositories.ChamadoRepository;
-import com.felipe.helpdesk.repositories.ClienteRepository;
-import com.felipe.helpdesk.repositories.TecnicoRepository;
-
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
+public class HelpdeskApplication {
 	
-	@Autowired
-	private ChamadoRepository chamadoRepository;
-	
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private TecnicoRepository tecnicoRepository; 
-
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Tecnico tecnico1 = new Tecnico(null, "Felipe Santos", "05062419042", "felipe@gmail.com", "123");
-		tecnico1.addPerfil(Perfil.ADMIN);
-		
-		Cliente cliente1 = new Cliente(null, "Linus Torvalds", "57683488089", "linus@gmail.com", "456");
-		
-		Chamado chamado1 = new Chamado(null, Prioridade.MEDIA, 
-				Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tecnico1, cliente1);
-		
-		tecnicoRepository.saveAll(Arrays.asList(tecnico1));
-		clienteRepository.saveAll(Arrays.asList(cliente1));
-		chamadoRepository.saveAll(Arrays.asList(chamado1));
-		
-	}
-
 }
