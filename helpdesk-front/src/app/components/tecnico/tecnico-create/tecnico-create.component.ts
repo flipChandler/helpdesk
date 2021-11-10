f8bc2143eb000af7407e4431183e1b6118cf3e00
flipChandler <felipe.santos11@hotmail.com>
2021-11-10 14:02:48 -0300

    create tecnico finalizado

--- helpdesk-front/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ helpdesk-front/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,19 +12,55 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class TecnicoCreateComponent implements OnInit {
 
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
   nome: FormControl = new FormControl(null, Validators.minLength(3));
   cpf: FormControl = new FormControl(null, Validators.required);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(private tecnicoService: TecnicoService,
+              private toast: ToastrService,
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  create(): void {
+    this.tecnicoService.create(this.tecnico).subscribe(response => {
+      this.toast.success('Técnico cadastrado com sucesso', 'Cadastro');
+      this.router.navigate(['tecnicos']);
+    }, exception => {
+      if (exception.error.errors) {
+        exception.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(exception.error.message);
+      }
+    });
+  }
+
+  addPerfil(perfilSelecionado: any): void {
+    if (this.tecnico.perfis.includes(perfilSelecionado)) { // se tecnico já tiver esse perfil
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfilSelecionado), 1); // tira esse perfil
+    } else {
+      this.tecnico.perfis.push(perfilSelecionado); // add esse perfil
+    }    
   }
 
   validaCampos(): boolean {
     return this.nome.valid && this.cpf.valid
       && this.email.valid && this.senha.valid;
   }
+
 
 }
